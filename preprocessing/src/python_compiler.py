862422c6497ab8f724a5c5e5bf20f9aa64d6dbde
Jeffrey Shen <jdshen@Jeffreys-MacBook-Pro-2.local>
2020-10-28 17:35:42 -0700

    add method to shard based on md5

--- preprocessing/src/python_compiler.py
+++ preprocessing/src/python_compiler.py
@@ -9,11 +9,13 @@ import py_compile
 import multiprocessing as mp
 import tempfile
 import dis
+import hashlib
+import contextlib
 
 
 def python_to_bytecode(python_code, filename="a.py", tmp_dir="/tmp", asm_format="dis"):
     try:
-        with io.StringIO() as output_buffer:    
+        with io.StringIO() as output_buffer:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 if asm_format == "dis":
@@ -23,11 +25,16 @@ def python_to_bytecode(python_code, filename="a.py", tmp_dir="/tmp", asm_format=
                     with open(path, "w") as file:
                         file.write(python_code)
 
-                    pyc_path = py_compile.compile(path, dfile=filename, doraise=True, optimize=0)
-                    disassemble_file(pyc_path, outstream=output_buffer, asm_format=asm_format)
+                    pyc_path = py_compile.compile(
+                        path, dfile=filename, doraise=True, optimize=0
+                    )
+                    disassemble_file(
+                        pyc_path, outstream=output_buffer, asm_format=asm_format
+                    )
             return output_buffer.getvalue()
     except:
         return None
+
 
 def python_to_bytecode_line(
     line, input_content, output_content, filter_none=True, **kwargs
@@ -36,7 +43,7 @@ def python_to_bytecode_line(
     if input_content in json_obj:
         bytecode = python_to_bytecode(json_obj[input_content], **kwargs)
     else:
-        bytecode = None 
+        bytecode = None
 
     if filter_none and bytecode is None:
         return None
@@ -95,7 +102,7 @@ def python_to_bytecode_dataset_helper(
     for line in input_file:
         input_queue.put(line)
         if progress_bar is not None:
-            progress_bar.update(len(line.encode("utf-8")))
+            progress_bar.update(len(line.encode()))
 
     for _ in range(num_processes):
         input_queue.put(None)
@@ -131,4 +138,58 @@ def python_to_bytecode_dataset(input_path, output_path, progress_bar=True, **kwa
                 pbar_to_pass = pbar if progress_bar else None
                 python_to_bytecode_dataset_helper(
                     input_file, output_file, progress_bar=pbar_to_pass, **kwargs
+                )
+
+
+def shard_dataset_helper(
+    input_file, output_files, input_content="content", progress_bar=None, **kwargs
+):
+    for line in input_file:
+        json_obj = json.loads(line)
+        content = json_obj[input_content] if input_content in json_obj else ""
+        hash_bytes = hashlib.md5(content.encode()).digest()
+        index = int.from_bytes(hash_bytes, byteorder="big", signed=False) % len(
+            output_files
+        )
+        output_files[index].write(line)
+
+        if progress_bar is not None:
+            progress_bar.update(len(line.encode()))
+
+
+def shard_dataset(
+    input_path, output_path_pattern, shards=8, progress_bar=True, **kwargs
+):
+    if isinstance(input_path, str):
+        input_path = pathlib.PurePath(input_path)
+    if isinstance(output_path_pattern, str):
+        output_path_pattern = pathlib.PurePath(output_path_pattern)
+    input_fn = gzip.open if (input_path.suffix == ".gz") else open
+    output_fn = gzip.open if (output_path_pattern.suffix == ".gz") else open
+
+    output_paths = [
+        output_path_pattern.with_name(output_path_pattern.name.replace("*", str(i)))
+        for i in range(shards)
+    ]
+
+    with input_fn(input_path, mode="rt") as input_file:
+        with contextlib.ExitStack() as stack:
+            output_files = [
+                stack.enter_context(output_fn(output_path, mode="wt"))
+                for output_path in output_paths
+            ]
+            # use a dummy __enter__, __exit__ if progress_bar is False
+            with (
+                tqdm.tqdm(
+                    total=pathlib.Path(input_path).stat().st_size,
+                    unit="B",
+                    unit_scale=True,
+                    unit_divisor=1024,
+                )
+                if progress_bar
+                else io.StringIO()
+            ) as pbar:
+                pbar_to_pass = pbar if progress_bar else None
+                shard_dataset_helper(
+                    input_file, output_files, progress_bar=pbar_to_pass, **kwargs
                 )
